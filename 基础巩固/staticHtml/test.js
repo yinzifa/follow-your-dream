f605885bef96737a8adc61614d7f1e6ff79ed183
yinzifa <yzf0224@qq.com>
2022-07-08 17:13:00 +0800

    主要是播放器

---
+++ 基础巩固/staticHtml/test.js
@@ -0,0 +1,24 @@
+let obj = {a: 1}
+let obj1 = obj;
+let obj2 = {a: 1}
+// let map = new Map();
+// map.set(JSON.stringify(obj),123)
+//
+// console.log(map)
+// console.log(map.has(JSON.stringify(obj2)))
+// map.set(obj2,456)
+// console.log(map.get(obj))
+// console.log(map.get(obj2))
+//
+// let objT = {}
+// objT[JSON.stringify(obj)] = 123;
+// objT[JSON.stringify(obj2)] = 456;
+// console.log(objT)
+
+
+
+/*
+let set = new Set()
+set.add(obj,567)
+console.log(set)
+console.log(set.has(obj2))*/
